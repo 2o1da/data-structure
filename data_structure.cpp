c8b4d309ddeb1eb01be16df94e9576077324ec35
solda <solda@khu.ac.kr>
2021-02-04 22:55:20 +0900

    updated

--- data_structure.cpp
+++ data_structure.cpp
@@ -231,3 +231,24 @@ int main()
 
     return 0;
 }
+
+#define LIST_LEN 100
+typedef int LData;
+
+typedef struct __ArrayList
+{
+    LData arr[LIST_LEN];
+    int numOfData;
+    int curPosition;
+} ArrayList;
+
+typedef ArrayList List;
+
+void ListInit(List *plist);
+void LInsert(List *plist, LData data);
+
+int LFirst(List *plist, LData *pdata);
+int LNext(List *plist, LData *pdata);
+
+LData LRemove(List *plist);
+int LCount(List *plist);
