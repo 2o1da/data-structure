2355ed981ff1f501d64cad28b8b4604609ce906a
solda <solda@khu.ac.kr>
2021-02-11 22:23:43 +0900

    updated

---
+++ data_structure4.cpp
@@ -0,0 +1,166 @@
+﻿#include <iostream>
+using namespace std;
+
+typedef int LData;
+
+typedef struct _node;
+{
+	LData data;
+	struct _node* next;
+} Node;
+
+typedef struct _linkedList
+{
+	Node* head;
+	Node* cur;
+	Node* before;
+	int numOfData;
+	int (*comp)(LData d1, LData d2);
+} LinkedList;
+
+typedef LinkedList List;
+
+void ListInit(List* plist)
+{
+	plist->head = (Node*)malloc(sizeof(Node));
+	plist->head->next = NULL;
+	plist->comp = NULL;
+	plist->numOfData = 0;
+}
+
+void LInsert(List* plist, LData data)
+{
+	if (plist->comp == NULL)
+		FInsert(plist, data);
+	else
+		SInsert(plist, data);
+}
+
+int LFirst(List* plist, LData* pdata)
+{
+	if (plist->head->next == NULL)
+		return false;
+
+	plist->before = plist->head;
+	plist->cur = plist->head->next;
+
+	*pdata = plist->cur->data;
+	return true;
+}
+
+int LNext(List* plist, LData* pdata)
+{
+	if (plist->cur->next == NULL)
+		return false;
+
+	plist->before = plist->cur;
+	plist->cur = plist->cur->next;
+
+	*pdata = plist->cur->data;
+	return true;
+
+}
+
+LData LRemove(List* plist)
+{
+	Node* rpos = plist->cur;
+	LData rdata = rpos->data;
+
+	plist->before->next = plist->cur->next;
+	plist->cur = plist->before;
+
+	free(rpos);
+	(plist->numOfData)--;
+	return rdata;
+}
+
+int LCount(List* plist)
+{
+	return plist->numOfData;
+}
+
+void SetSortRule(List* plist, int(*comp)(LData d1, LData d2));
+
+
+void FInsert(List* plist, LData data)
+{
+	Node* newNode = (Node*)malloc(sizeof(Node));
+	newNode->data = data;
+
+	newNode->next = plist->head->next;
+	plist->head->next = newNode;
+
+	(plist->numOfData)++;
+}
+
+void SInsert(List* plist, LData data)
+{
+
+}
+
+
+int main()
+{
+	List list;
+	int data;
+	ListInit(&list);
+
+	LInsert(&list, 11); LInsert(&list, 11);
+	LInsert(&list, 22); LInsert(&list, 22);
+	LInsert(&list, 33);
+
+	cout << "현재 데이터의 수 :" << LCount(&list) << endl;
+
+	if (LFirst(&list, &data))
+	{
+		cout << data << endl;
+
+		while(LNext(&list, &data))
+		{
+			cout << data << endl;
+		}
+	}
+
+	cout << endl << endl;
+
+	if (LFirst(&list, &data))
+	{
+		if (data == 22)
+			LRemove(&list);
+
+		while (LNext(&list, &data))
+		{
+			if (data == 22)
+				LRemove(&list);
+		}
+	}
+
+	cout << "현재 데이터의 수 :" << LCount(&list) << endl;
+
+	if (LFirst(&list, &data))
+	{
+		cout << data << endl;
+
+		while (LNext(&list, &data))
+		{
+			cout << data << endl;
+		}
+	}
+
+	cout << endl << endl;
+
+	return 0;
+}
+
+
+
+// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
+// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
+
+// 시작을 위한 팁: 
+//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
+//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
+//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
+//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
+//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
+//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
