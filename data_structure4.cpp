3beca53cd421ef0bc3d46c377fd30a7e4a5d0a89
solda <solda@khu.ac.kr>
2021-02-12 22:51:40 +0900

    updated

--- data_structure4.cpp
+++ data_structure4.cpp
@@ -152,6 +152,68 @@ int main()
 	return 0;
 }
 
+int WhoIsPrecede(int d1, int d2)
+{
+	if (d1 < d2)
+		return 0;
+	else
+		return 1;
+}
+
+int main()
+{
+	List list;
+	int data;
+	ListInit(&list);
+
+	SetSortRule(&list, WhoIsPrecede);
+
+	LInsert(&list, 11); LInsert(&list, 11);
+	LInsert(&list, 22); LInsert(&list, 22);
+	LInsert(&list, 33);
+
+	cout << "현재 데이터의 수 :" << LCount(&list) << endl;
+
+	if (LFirst(&list, &data))
+	{
+		cout << data << endl;
+
+		while (LNext(&list, &data))
+		{
+			cout << data << endl;
+		}
+	}
+
+	cout << endl << endl;
+
+	if (LFirst(&list, &data))
+	{
+		if (data == 22)
+			LRemove(&list);
+
+		while (LNext(&list, &data))
+		{
+			if (data == 22)
+				LRemove(&list);
+		}
+	}
+
+	cout << "현재 데이터의 수 :" << LCount(&list) << endl;
+
+	if (LFirst(&list, &data))
+	{
+		cout << data << endl;
+
+		while (LNext(&list, &data))
+		{
+			cout << data << endl;
+		}
+	}
+
+	cout << endl << endl;
+
+	return 0;
+}
 
 
 // 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
