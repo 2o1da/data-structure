151074adb99948d7a758dceb88988927aa1f6b69
solda <solda@khu.ac.kr>
2021-02-14 23:21:58 +0900

    updated

--- data_structure5.cpp
+++ data_structure5.cpp
@@ -1,6 +1,123 @@
 ﻿#include <iostream>
 using namespace std;
 
+typedef int Data;
+
+typedef struct _node
+{
+	Data data;
+	struct _node* next;
+} Node;
+
+typedef struct _CLL
+{
+	Node* tail;
+	Node* cur;
+	Node* before;
+	int numOfData;
+}CList;
+
+typedef CList List;
+
+void ListInit(List* plist)
+{
+	plist->tail = NULL;
+	plist->cur = NULL;
+	plist->before = NULL;
+	plist->numOfData = 0;
+}
+
+void LInsert(List* plist, Data data)
+{
+	Node* newNode = (Node*)malloc(sizeof(Node));
+	newNode->data = data;
+
+	if (plist->tail == NULL)
+	{
+		plist->tail = newNode;
+		newNode->next = newNode;
+	}
+	else
+	{
+		newNode->next = plist->tail->next;
+		plist->tail->next = newNode;
+		plist->tail = newNode;
+	}
+	
+	(plist->numOfData)++;
+}
+
+void LInsertFront(List* plist, Data data)
+{
+	Node* newNode = (Node*)malloc(sizeof(Node));
+	newNode->data = data;
+
+	if (plist->tail == NULL)
+	{
+		plist->tail = newNode;
+		newNode->next = newNode;
+	}
+	else
+	{
+		newNode->next = plist->tail->next;
+		plist->tail->next = newNode;
+	}
+
+	(plist->numOfData)++;
+}
+
+int LFirst(List* plist, Data* pdata)
+{
+	if (plist->tail == NULL)
+		return false;
+
+	plist->before = plist->tail;
+	plist->cur = plist->tail->next;
+
+	*pdata = plist->cur->data;
+	return true;
+}
+
+int LNext(List* plist, Data* pdata)
+{
+	if (plist->tail == NULL)
+		return false;
+
+	plist->before = plist->cur;
+	plist->cur = plist->cur->next;
+
+	*pdata = plist->cur->data;
+	return true;
+}
+
+Data LRemove(List* plist)
+{
+	Node* rpos = plist->cur;
+	Data rdata = rpos->data;
+
+	if (rpos == plist->tail)
+	{
+		if (plist->tail == plist->tail->next)
+			plist->tail = NULL;
+		else
+			plist->tail = plist->before;
+	}
+
+	plist->before->next = plist->cur->next;
+
+	plist->cur = plist->before;
+
+	free(rpos);
+	(plist->numOfData)--;
+	return rdata;	
+}
+
+int LCount(List* plist)
+{
+
+}
+
+
 int main()
 {
 
