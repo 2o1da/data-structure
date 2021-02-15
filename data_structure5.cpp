9d1ad58e15a588340f9ee31669d87db13594d285
solda <solda@khu.ac.kr>
2021-02-15 23:07:37 +0900

    updated

--- data_structure5.cpp
+++ data_structure5.cpp
@@ -114,13 +114,61 @@ Data LRemove(List* plist)
 
 int LCount(List* plist)
 {
-
+	return plist->numOfData;
 }
 
 
 int main()
 {
+	List list;
+	int data, i, nodeNum;
+	ListInit(&list);
 
+	LInsert(&list, 3);
+	LInsert(&list, 4);
+	LInsert(&list, 5);
+	LInsertFront(&list, 2);
+	LInsertFront(&list, 1);
+
+	if (LFirst(&list, &data))
+	{
+		cout << data << endl;
+
+		for (i = 0; i < LCount(&list) * 3 - 1; i++)
+		{
+			if (LNext(&list, &data))
+				cout << data << endl;
+		}
+	}
+	cout << endl;
+
+	nodeNum = LCount(&list);
+
+	if (nodeNum != 0)
+	{
+		LFirst(&list, &data);
+		if (data % 2 == 0)
+			LRemove(&list);
+
+		for (i = 0; i < nodeNum - 1; i++)
+		{
+			LNext(&list, &data);
+			if (data % 2 == 0)
+				LRemove(&list);
+		}
+	}
+
+	if (LFirst(&list, &data))
+	{	
+		cout << data << endl;
+		
+		for (i = 0; i < LCount(&list) * 3 - 1; i++)
+		{
+			if (LNext(&list, &data))
+				cout << data << endl;
+		}
+	}
+	return 0;
 }
 
 
