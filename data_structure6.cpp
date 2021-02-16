a1aae1fd6478f49e8ec963c66c340d1949dad655
solda <solda@khu.ac.kr>
2021-02-16 19:52:39 +0900

    updated

---
+++ data_structure6.cpp
@@ -0,0 +1,124 @@
+﻿#include <iostream>
+using namespace std;
+
+typedef int Data;
+
+typedef struct _node
+{
+	Data data;
+	struct _node* next;
+	struct _node* prev;
+} Node;
+
+typedef struct _DLinkedList
+{
+	Node* head;
+	Node* cur;
+	int numOfData;
+} DBLinkedList;
+
+typedef DBLinkedList List;
+
+void ListInit(List* plist)
+{
+	plist->tail = NULL;
+	plist->cur = NULL;
+	plist->before = NULL;
+	plist->numOfData = 0;
+}
+
+void LInsert(List* plist, Data data)
+{
+	Node* newNode = (Node*)malloc(sizeof(Node));
+	newNode->data = data;
+
+	newNode->next = plist->head;
+
+	if (plist->head != NULL)
+		plist->head->prev = newNode;
+
+	newNode->prev = NULL;
+	plist->head = newNode;
+	
+	(plist->numOfData)++;
+}
+
+int LFirst(List* plist, Data* pdata)
+{
+	if (plist->tail == NULL)
+		return false;
+
+	plist->cur = plist->head;
+	plist->cur = plist->tail->next;
+
+	*pdata = plist->cur->data;
+	return true;
+}
+
+int LNext(List* plist, Data* pdata)
+{
+	if (plist->cur->next == NULL)
+		return false;
+
+	plist->cur = plist->cur->next;
+	*pdata = plist->cur->data;
+	return true;
+}
+
+int LPrevious(List* plist, Data* pdata)
+{
+	if (plist->cur->prev == NULL)
+		return false;
+
+	plist->cur = plist->cur->prev;
+	*pdata = plist->cur->data;
+	return true;
+}
+
+int LCount(List* plist)
+{
+	return plist->numOfData;
+}
+
+int main()
+{
+	List list;
+	int data;
+	ListInit(&list);
+
+	LInsert(&list, 1);
+	LInsert(&list, 2);
+	LInsert(&list, 3);
+	LInsert(&list, 4);
+	LInsert(&list, 5);
+	LInsert(&list, 6);
+	LInsert(&list, 7);
+	LInsert(&list, 8);
+	
+	if (LFirst(&list, &data))
+	{
+		cout << data << endl;
+
+		while (LNext(&list, && data))
+			cout << data << endl;
+
+		while (LPrevious(&list, &data))
+			cout << data << endl;
+
+		cout << endl << endl;
+	}
+
+	return 0;
+}
+
+
+// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
+// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
+
+// 시작을 위한 팁: 
+//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
+//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
+//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
+//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
+//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
+//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
