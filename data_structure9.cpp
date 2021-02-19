a1bdb3e7f16ab68d0352c8ccdd059fd92ea94725
solda <solda@khu.ac.kr>
2021-02-19 09:22:40 +0900

    updated

---
+++ data_structure9.cpp
@@ -0,0 +1,187 @@
+﻿#include <iostream>
+#include
+using namespace std;
+
+int GetOpPrec(char op)
+{
+	switch (op)
+	{
+	case'*':
+	case'/':
+		return 5;
+	case'+':
+	case'-':
+		return 3;
+	case'(':
+		return 1;
+	}
+	return -1;
+}
+
+int WhoPrecOp(char op1, char op2)
+{
+	int op1Prec = GetOpPrec(op1);
+	int op2Prec = GetOpPrec(op2);
+
+	if (op1Prec > op2Prec)
+		return 1;
+	elseif(op1Prec < op2Prec)
+		return -1;
+	else
+		return 0;
+}
+
+void ConvToRPNExp(char exp[])
+{
+	Stack stack;
+	int expLen = strlen(exp);
+	char* convExp = (char*)malloc(expLen + 1);
+
+	int i, idx = 0;
+	char tok, popOp;
+
+	memset(convExp, 0, sizeof(char) * expLen + 1);
+	StackInit(&stack);
+
+	for (i = 0; i < expLen; i++)
+	{
+		tok = exp[i];
+		if (isdigit(tok))
+		{
+			convExp[idx++] = tok;
+		}
+		else
+		{
+			switch (tok)
+			{
+			case'(':
+				SPush(&stack, tok);
+				break;
+
+			case')':
+				while (1)
+				{
+					popOp = SPop(&stack);
+					if (popOp == '(')
+						break;
+					convExp[idx++] = popOp;
+				}
+				break;
+
+			case'+':case'-':
+			case'*':case'/':
+				while (!SIsEmpty(&stack) &&
+					WhoPrecOp(SPeek(&stack), tok) >= 0)
+					convExp[idx++] = SPop(&stack);
+
+				SPush(&stack, tok);
+				break;
+			}
+		}
+	}
+
+	while (!SIsEmpty(&stack))
+		convexp[idx++] = SPop(&stack);
+
+	strcpy(exp, convExp);
+	free(convExp);
+	free(convExp);
+}
+
+int main()
+{
+	char exp1[] = "1+2*3";
+	char exp2[] = "(1+2)*3";
+	char exp3[] = "((1-2)+3)*(5-2)";
+
+	ConvToRPNExp(exp1);
+	ConvToRPNExp(exp2);
+	ConvToRPNExp(exp3);
+
+	cout << exp1 << endl;
+	cout << exp2 << endl;
+	cout << exp3 << endl;
+
+	return 0;
+}
+
+// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
+// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
+
+// 시작을 위한 팁: 
+//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
+//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
+//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
+//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
+//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
+//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
+
+typedef int Data;
+
+typedef struct _node
+{
+	Data data;
+	struct _node* next;
+} Node;
+
+typedef struct _listStack
+{
+	Node* head;
+}ListStack;
+
+typedef ListStack Stack;
+
+
+void StackInit(Stack* pstack)
+{
+	pstack->head = NULL;
+}
+
+void SIsEmpty(Stack* pstack)
+{
+	if (pstack->head == NULL)
+		return 1;
+	else
+		return 0;
+}
+
+void SPush(Stack* pstack, Data data)
+{
+	Node* newNode = (Node*)malloc(sizeof(Node));
+
+	newNode->data = data;
+	newNode->next = pstack->head;
+
+	pstack->head = newNode;
+}
+
+Data SPop(Stack* pstack)
+{
+	Data rdata;
+	Node* rnode;
+
+	if (SIsEmpty(pstack))
+	{
+		cout << "스택 메모리 에러\n";
+		exit(-1);
+	}
+
+	rdata = pstack->head->data;
+	rnode = pstack->head;
+
+	pstack->head = pstack->head->next;
+	free(rnode);
+
+	return rdata;
+}
+
+Data SPeek(Stack* pstack)
+{
+	if (SIsEmpty(pstack))
+	{
+		cout << "스택 메모리 에러\n";
+		exit(-1);
+	}
+
+	return pstack->head->data;
+}
